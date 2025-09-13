e948d8136b3bee546b347f3ebdaa4255adfefc9f
Ole Engel <engel@xrg-simulation.de>
2025-09-13 23:30:44 +0200

    test(controller): remove redundant imports in `AuthControllerTest`
    - Cleanup unused `@ExtendWith(MockitoExtension.class)` annotation for simplicity.

--- src/test/java/com/sme/afs/controller/AuthControllerTest.java
+++ src/test/java/com/sme/afs/controller/AuthControllerTest.java
@@ -9,8 +9,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
