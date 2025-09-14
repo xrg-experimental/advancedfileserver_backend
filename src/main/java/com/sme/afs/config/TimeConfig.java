6de45c55a565f8194082c131cad840a71fd02c44
Ole Engel <engel@xrg-simulation.de>
2025-09-14 10:22:16 +0200

    fix(time-config): update `Clock` bean to use UTC with conditional primary setup
    - Change default `Clock` implementation to `Clock.systemUTC()` for consistent time zone usage.
    - Add `@Primary` and `@ConditionalOnMissingBean` annotations for better configurability.

--- src/main/java/com/sme/afs/config/TimeConfig.java
+++ src/main/java/com/sme/afs/config/TimeConfig.java
@@ -1,7 +1,9 @@
 package com.sme.afs.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.time.Clock;
 
@@ -9,7 +11,9 @@ import java.time.Clock;
 public class TimeConfig {
 
     @Bean
+    @Primary
+    @ConditionalOnMissingBean(Clock.class)
     public Clock systemClock() {
-        return Clock.systemDefaultZone();
+        return Clock.systemUTC();
     }
 }
